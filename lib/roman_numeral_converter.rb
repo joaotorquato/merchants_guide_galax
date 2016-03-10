c2d22cc8b19417ee9139d3c9ebee2d443184edd4
João Torquato <joao.otl@gmail.com>
2016-03-09 21:48:47 -0300

    first commit

--- lib/roman_numeral_converter.rb
+++ lib/roman_numeral_converter.rb
@@ -1,5 +1,18 @@
 class RomanNumeralConverter
   def convert(number)
-    'I' * number
+    total = String.new
+    return 'L' if number == 50
+    return 'XL' if number == 40
+    while number >= 10
+      total += 'X'
+      number -= 10
+    end
+    if number >= 9
+      total += 'IX'
+      number -= 9
+    end
+    return 'V' if number == 5
+    return 'IV' if number == 4
+    total += 'I' * number
   end
 end
--- spec/roman_numerals_converter_spec.rb
+++ spec/roman_numerals_converter_spec.rb
@@ -15,4 +15,40 @@ RSpec.describe RomanNumeralConverter, '#convert' do
     expect(converter.convert(3)).to eq 'III'
   end
 
+  it 'calculates the roman number for 4' do
+    expect(converter.convert(4)).to eq 'IV'
+  end
+
+  it 'calculates the roman number for 5' do
+    expect(converter.convert(5)).to eq 'V'
+  end
+
+  it 'calculates the roman number for 9' do
+    expect(converter.convert(9)).to eq 'IX'
+  end
+
+  it 'calculates the roman number for 10' do
+    expect(converter.convert(10)).to eq 'X'
+  end
+
+  it 'calculates the roman number for 20' do
+    expect(converter.convert(20)).to eq 'XX'
+  end
+
+  it 'calculates the roman number for 30' do
+    expect(converter.convert(30)).to eq 'XXX'
+  end
+
+  it 'calculates the roman number for 39' do
+    expect(converter.convert(39)).to eq 'XXXIX'
+  end
+
+  it 'calculates the roman number for 40' do
+    expect(converter.convert(40)).to eq 'XL'
+  end
+
+  it 'calculates the roman number for 50' do
+    expect(converter.convert(50)).to eq 'L'
+  end
+
 end
